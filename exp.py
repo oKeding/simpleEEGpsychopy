4a3607248f4e37801321b74374a156228d5d5dc7
oKeding <61871273+oKeding@users.noreply.github.com>
2024-10-22 17:19:40 +0200

    Create exp.py

---
+++ exp.py
@@ -0,0 +1,17 @@
+# Save folder.
+
+# Set up trigger LSL.
+x = 0
+# Set up AudioCapture LSL stream.
+
+# Set up sound player.
+
+# Set up monitor.
+
+# In chunks of 3 mins: 
+    # Make fixation cross.
+    # Load audio [choose yourself, a podcast].
+    #LSL trigger.
+    #podcast start.
+    # Save data to trial audio.
+    # Tell participant of break.
